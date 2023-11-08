0bc2355ece27077d367111579d8c1a72e442e926
Luís <luisphilipedantasmaia@gmail.com>
2023-11-08 18:46:10 -0300

    Initial commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 7;
+    public bool noChao;
+
     private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         TryGetComponent(out rb);
     }
+
+     private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChao && collision.gameObject.tag == "Chao")
+        {
+            noChao = true;
+        }
+    }
+    
 
     // Update is called once per frame
     void Update()
@@ -19,6 +32,22 @@ public class Player : MonoBehaviour
         float v = Input.GetAxis("Vertical"); //-1 trás, 0 nada, 1 frente
 
         Vector3 direcao = new Vector3(h, 0, v);
-        rb.AddForce(direcao * velocidade * Time.deltaTime);
+        rb.AddForce(direcao * velocidade * Time.deltaTime,ForceMode.Impulse);
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
+
+
+
+
+
+        if(transform.position.y <= -10)
+        {
+            //jogador caiu
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
